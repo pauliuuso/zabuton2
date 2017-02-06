c15a917e34d397f67f5a8d636c773c29e36594b8
silent-hero <p.jazauskas@gmail.com>
2017-02-06 22:25:57 +0200

    added title

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour
+{
+
+    private int rotateSpeed = 2;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(gameObject.transform.localEulerAngles.y <= 170f)
+        {
+            rotateSpeed *= -1;
+        }
+        else if(gameObject.transform.localEulerAngles.y >= 190f)
+        {
+            rotateSpeed *= -1;
+        }
+        gameObject.transform.Rotate(0, 0, Time.deltaTime * rotateSpeed);
+    }
+}
