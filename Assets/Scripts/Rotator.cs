c7a6dba8422c7eb8a71d86c57de7b6e8ad65915a
silent-hero <p.jazauskas@gmail.com>
2017-02-07 22:43:43 +0200

    added start button

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour 
+{
+
+    public float speed = 2f;
+    public bool rotateX = false;
+    public bool rotateY = false;
+    public bool rotateZ = false;
+	
+
+	void FixedUpdate () 
+    {
+        if (rotateX) transform.localEulerAngles += Vector3.right * Time.deltaTime * speed;
+        if (rotateY) transform.localEulerAngles += Vector3.up * Time.deltaTime * speed;
+        if (rotateZ) transform.localEulerAngles += Vector3.forward * Time.deltaTime * speed;
+	}
+}
---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
+{
+	public Sprite inactive;
+	public Sprite active;
+
+	void Start()
+	{
+
+	}
+	
+	public void OnPointerDown(PointerEventData eventData)
+	{
+
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		transform.GetComponent<Image>().overrideSprite = active;
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		transform.GetComponent<Image>().overrideSprite = inactive;
+	}
+
+}
