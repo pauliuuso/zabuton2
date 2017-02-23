6cb6dc5302480159b5c24cedb7f70ccca7609d85
silent-hero <p.jazauskas@gmail.com>
2017-02-23 23:11:47 +0200

    finished doge scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour, IPointerDownHandler
+{
+    public string goTo;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SceneManager.LoadScene(goTo, LoadSceneMode.Single);
+    }
+}
