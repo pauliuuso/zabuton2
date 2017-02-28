8fe3376554414a38fcaef516523a048c9e5af3e0
silent-hero <p.jazauskas@gmail.com>
2017-02-28 21:24:18 +0200

    added fog

--- Assets/Scripts/PlanetChooser.cs
+++ Assets/Scripts/PlanetChooser.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlanetChooser : MonoBehaviour 
 {
     public GameObject cameraPoint;
     public float distanceFromClosePlanet = 5f;
     public GameObject invadeUI;
+    public List<GameObject> particleEffects;
+    public GameObject particlePoint;
 
     private Ray ray;
     private Camera camera;
     private RaycastHit hit;
+    private RaycastHit previousHit;
 
 	void Start () 
     {
@@ -27,7 +31,7 @@ public class PlanetChooser : MonoBehaviour
             }
             else
             {
-                print("hit");
+
             }
         }
 
@@ -36,7 +40,18 @@ public class PlanetChooser : MonoBehaviour
         {
             cameraPoint.transform.position = hit.transform.position + new Vector3(distanceFromClosePlanet, 0, 0f);
             invadeUI.gameObject.SetActive(true);
+            if(previousHit.transform == null || previousHit.transform.position != hit.transform.position && hit.transform.tag == "Planet")
+            {
+                addParticles(0);
+            }
+            previousHit = hit;
         }
 
 	}
+
+    public void addParticles(int numb)
+    {
+        GameObject particles = Instantiate(particleEffects[numb], particlePoint.transform.position, particlePoint.transform.rotation) as GameObject;
+        particles.transform.parent = transform;
+    }
 }
